6097b4d1cb15bc0a8c0eb23e99255dd42397d437
Yann <yannm.dev@gmail.com>
2023-07-09 21:29:53 -0300

    Remove unused assets and add powered by coffe icon

--- src/pages/Home/Footer/index.js
+++ src/pages/Home/Footer/index.js
@@ -1,11 +1,13 @@
 /* eslint-disable max-len */
 import React from 'react'
-import { Container } from './styles'
+import PoweredByCoffee from 'assets/images/poweredbycoffee.png'
+import { Container, StyledImage } from './styles'
 import SocialMedia from '../Header/SocialMedia'
 
 export const Footer = () => (
   <Container>
     <SocialMedia />
+    <StyledImage src={PoweredByCoffee} alt="" />
   </Container>
 )
 
--- src/pages/Home/Footer/styles.js
+++ src/pages/Home/Footer/styles.js
@@ -3,8 +3,14 @@ import { colors } from 'app/globals/colors'
 
 export const Container = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   background-color: ${colors.backgrounds.black};
   justify-content: center;
+  align-items: center;
+  gap: 10px;
   padding: 2rem;
+`
+
+export const StyledImage = styled.img`
+  max-width: 20vh;
 `
